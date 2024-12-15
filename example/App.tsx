c233af48a9e962c377a5fdec584c53d830ba1806
velly <fanxing1993@qq.com>
2024-12-15 21:13:29 +0800

    chore: save

--- example/App.tsx
+++ example/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useIntercept } from './hooks/intercept';
 import { usePagingSetting } from './hooks/pagingSetting';
