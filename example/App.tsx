c610ed6e0adfae8d836c0c336320386c778f21b9
wxl <fanxing1993@qq.com>
2025-02-24 16:51:11 +0800

    chore: save

--- example/App.tsx
+++ example/App.tsx
@@ -1,26 +1,20 @@
 import React, { useContext, useEffect } from 'react';
 import './App.css';
-import { useLoad } from '../packages/common/useLoad';
-import { createComponent, useStash, $, useInjector, useComputed, Render, useWatchEffect } from '../packages';
+import { useStash, Render } from '../packages';
 
-// 根组件调用createComponent函数，将hook函数作为provider传入
 const App = () => {  
   const [count, setCount] = useStash(0); 
 
   return (
-    <>
-      <div>{new Date().getTime()}</div>
-      <Render>
-        {
-          () => 
-            <>            
-              <div>父组件：{count()}</div>
-              <button onClick={() => setCount(v => v+1)}>+</button>
-            </> 
-        }
-      </Render>
-    </>
-    
+    <Render>
+      {
+        () => 
+          <>            
+            <div>父组件：{count()}</div>
+            <button onClick={() => setCount(v => v+1)}>+</button>
+          </> 
+      }
+    </Render>    
   )
 }
 
